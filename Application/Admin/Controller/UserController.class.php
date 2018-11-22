7bddb03ba73bcaa9299b1a1ab57b8bfca113a345
huangweimin <smile@huangweimindeMacBook-Pro.local>
2018-11-22 11:12:25 +0800

    1、修复后台图片上传问题 2、优化后台用户修改密码

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -6,7 +6,7 @@ use Think\Controller;
 class UserController extends BaseController
 {
     /**
-     * 获取留言列表
+     * 获取列表
      */
     public function index()
     {
@@ -20,7 +20,7 @@ class UserController extends BaseController
     }
 
     /**
-     * 删除留言
+     * 删除
      */
     public function del()
     {
@@ -30,6 +30,56 @@ class UserController extends BaseController
         $result = D('User')->Del($param);
 
         $this->ajaxReturn($result);
+
+    }
+
+    /**
+     * 新增/修改
+     */
+    public function create()
+    {
+
+        $do = I('do');
+
+        if (empty($do)) {
+
+            $title = "创建用户";
+
+            $param = I();
+
+            if ($param){
+
+                $title = "修改用户";
+
+                $map['id'] = $param['id'];
+
+                $detail = M('user')->where($map)->order('id desc')->find();
+
+                $this->assign('detail', $detail);
+           
+            }
+
+            $this->assign('title', $title);
+
+            $this->display();
+
+        } elseif ($do == 'create') {
+
+            $param = I();
+
+            $result = D('User')->Create($param);
+
+            $this->ajaxReturn($result);
+
+        }  elseif ($do == 'edit') {
+
+            $param = I();
+
+            $result = D('User')->Edit($param);
+
+            $this->ajaxReturn($result);
+
+        }
 
     }
 
--- Application/Admin/Model/UserModel.class.php
+++ Application/Admin/Model/UserModel.class.php
@@ -23,6 +23,111 @@ class UserModel extends Model
     }
 
     /**
+     * 创建
+     */
+    public function Create($param)
+    {
+
+    	$model = M('user');
+
+    	// 判断是否存在
+    	$flag = $model->where(['username' => $param['username'], 'is_deleted' => 0])->find();
+
+        if ($flag) {
+
+            return [
+
+                'data' => $param['username'] . '已经存在了',
+
+                'msg' => $model->getLastSql(),
+
+                'status' => 0,
+
+            ];
+
+        }
+
+
+        $doAdd = false;
+
+        $doAdd = $model->add([
+
+            'username' => $param['username'],
+
+            'password' => md5($param['password']),
+
+            'status' => 1,
+
+            'create_time' => date('Y-m-d H:i:s'),
+
+        ]);
+
+        $res = $doAdd ? ['msg' => 'success'] : ['msg' => 'failed'];
+
+        return [
+
+            'data' => $res['msg'],
+
+            'msg' => $model->getLastSql(),
+
+            'status' => $doAdd ? 1 : 0,
+
+        ];
+
+    }
+
+    /**
+     * 修改
+     */
+     public function Edit($param)
+    {
+
+    	$model = M('user');
+
+    	// 判断是否存在
+    	$flag = $model->where(['username' => $param['username'], 'is_deleted' => 0, 'id' => ['neq', $param['id']]])->find();
+
+        if ($flag) {
+
+            return [
+
+                'data' => $param['username'] . '已经存在了',
+
+                'msg' => $model->getLastSql(),
+
+                'status' => 0,
+
+            ];
+
+        }
+
+        $doMod = false;
+
+        $doMod = $model
+
+            ->where(['id' => $param['id']])
+
+        	->save([
+
+            'password' => md5($param['password'])
+
+        ]);
+
+        $res = ['msg' => 'success'] ;
+
+        return [
+
+            'data' => $res['msg'],
+
+            'msg' => $model->getLastSql(),
+
+            'status' =>  1,
+
+        ];
+
+    }
+
+    /**
      * 删除
      */
   	  public function Del($param)
